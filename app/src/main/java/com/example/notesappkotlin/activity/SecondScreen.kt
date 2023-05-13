209fa6c795818180f1ce12c44110940e7ce675f6
Giuseppe Barone <giuseppebarone2028@gmail.com>
2023-05-13 11:45:47 +0200

    ho aggiunto il comportamento ai bottoni dei colori; e ho sistemanto un po lo style della seconda schermata

--- app/src/main/java/com/example/notesappkotlin/activity/SecondScreen.kt
+++ app/src/main/java/com/example/notesappkotlin/activity/SecondScreen.kt
@@ -11,5 +11,35 @@ class SecondScreen : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivitySecondScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        clickBottoni()
+    }
+
+    private fun clickBottoni() {
+        //qui inserisco tutti i click dei bottoni
+        binding.button2.setOnClickListener {
+            binding.sfondoSecondScreen.background = resources.getDrawable(R.color.quarto)
+            binding.editTitle.setHintTextColor(resources.getColor(R.color.black))
+            binding.editTextText.setHintTextColor(resources.getColor(R.color.black))
+        }
+        binding.button6.setOnClickListener {
+            binding.sfondoSecondScreen.background = resources.getDrawable(R.color.quinto)
+            binding.editTitle.setHintTextColor(resources.getColor(R.color.black))
+            binding.editTextText.setHintTextColor(resources.getColor(R.color.black))
+        }
+        binding.button7.setOnClickListener {
+            binding.sfondoSecondScreen.background = resources.getDrawable(R.color.primo)
+            binding.editTitle.setHintTextColor(resources.getColor(R.color.black))
+            binding.editTextText.setHintTextColor(resources.getColor(R.color.black))
+        }
+        binding.button8.setOnClickListener {
+            binding.sfondoSecondScreen.background = resources.getDrawable(R.color.secondo)
+            binding.editTitle.setHintTextColor(resources.getColor(R.color.black))
+            binding.editTextText.setHintTextColor(resources.getColor(R.color.black))
+        }
+        binding.button9.setOnClickListener {
+            binding.sfondoSecondScreen.background = resources.getDrawable(R.color.terzo)
+            binding.editTitle.setHintTextColor(resources.getColor(R.color.black))
+            binding.editTextText.setHintTextColor(resources.getColor(R.color.black))
+        }
     }
 }
